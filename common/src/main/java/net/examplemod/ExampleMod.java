bbcb5b07301bdc44c0ca2f926f65da7000563c38
assiquattro4 <lorenzo.montagna.2009.01@gmail.com>
2026-03-26 12:33:19 +0100

    Refactor ExampleMod and register ModBlocks
    Updated logger initialization and added ModBlocks registration.

--- common/src/main/java/net/examplemod/ExampleMod.java
+++ common/src/main/java/net/examplemod/ExampleMod.java
@@ -1,22 +1,18 @@
 package net.examplemod;
 
-import com.simibubi.create.Create;
-import net.minecraft.resources.ResourceLocation;
+import net.examplemod.registry.ModBlocks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
-
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     public static void init() {
-        LOGGER.info("{} initializing! Create version: {} on platform: {}", NAME, Create.VERSION, ExampleExpectPlatform.platformName());
-        ExampleBlocks.init(); // hold registrate in a separate class to avoid loading early on forge
-    }
+        // AGGIUNGI QUESTA RIGA QUI SOTTO:
+        // Questo comando dice al gioco di caricare il nostro Redstone Track all'avvio
+        ModBlocks.register();
 
-    public static ResourceLocation id(String path) {
-        return new ResourceLocation(MOD_ID, path);
+        LOGGER.info("Redstone Track Addon caricato con successo!");
     }
 }
