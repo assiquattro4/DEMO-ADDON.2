ad659de807ea3dfffc5047d9d01f144f58cbd6e3
assiquattro4 <lorenzo.montagna.2009.01@gmail.com>
2026-03-26 12:40:24 +0100

    Add Redstone Track block registration
    Register Redstone Track block with specific properties.

--- common/src/main/java/net/examplemod/ExampleMod.java
+++ common/src/main/java/net/examplemod/ExampleMod.java
@@ -7,7 +7,16 @@ import org.slf4j.LoggerFactory;
 public class ExampleMod {
     public static final String MOD_ID = "examplemod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+    public static final Block REDSTONE_TRACK = Registry.register(
+        BuiltInRegistries.BLOCK,
+        new ResourceLocation("examplemod", "redstone_track"),
+        new RedstoneTrackBlock(BlockBehaviour.Properties.of()
+            .noCollission() // Il treno e i giocatori ci passano attraverso (come un binario vero)
+            .noOcclusion()  // Non blocca la luce e permette di vedere cosa c'è sotto
+            .dynamicShape() // Fondamentale per le contraption di Create
+            .strength(1.5f)
+        )
+    );
     public static void init() {
         // AGGIUNGI QUESTA RIGA QUI SOTTO:
         // Questo comando dice al gioco di caricare il nostro Redstone Track all'avvio
